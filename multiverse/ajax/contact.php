f7df9df0a70527047cd29a1c1fe8fcbf8f467d03
bic-ed <ranesi@tiscali.it>
2022-11-20 16:02:21 +0100

    Updated paths following ZP changes

--- multiverse/ajax/contact.php
+++ multiverse/ajax/contact.php
@@ -2,8 +2,8 @@
 define('IS_AJAX', isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest');
 if(!IS_AJAX) die('Restricted access');
 
-require_once(dirname(__FILE__, 4) . '/zp-core/functions.php'); // PHP 7+
-require_once(SERVERPATH . "/" . ZENFOLDER . '/functions-rewrite.php'); // renamed in ZP 1.6
+require_once(dirname(__FILE__, 4) . '/zp-core/functions/functions.php'); // PHP 7+
+require_once(SERVERPATH . "/" . ZENFOLDER . '/functions/functions-rewrite.php'); // renamed in ZP 1.6
 
 require_once(SERVERPATH . "/" . THEMEFOLDER . '/multiverse/functions.php');
 
